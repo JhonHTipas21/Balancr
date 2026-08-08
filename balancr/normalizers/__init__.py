e66373dfbd0b2af4de52d375687f636afd319089
JhonHTipas21 <168132558+JhonHTipas21@users.noreply.github.com>
2026-08-08 12:39:32 -0500

    feat: define internal ledger transaction source interface

--- balancr/normalizers/__init__.py
+++ balancr/normalizers/__init__.py
@@ -1,5 +1,7 @@
 from balancr.normalizers.base import TransactionSource
 from balancr.normalizers.gateway_csv import GatewayCSVSource
 from balancr.normalizers.bank_csv import BankCSVSource
+from balancr.normalizers.ledger_db import LedgerDBSource
 
-__all__ = ["TransactionSource", "GatewayCSVSource", "BankCSVSource"]
+__all__ = ["TransactionSource", "GatewayCSVSource", "BankCSVSource", "LedgerDBSource"]
+
